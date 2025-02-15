6ed3af9dce498e9e3a32c386f15df43c3ce835c3
Dylan Vu <96007994+Dyong46@users.noreply.github.com>
2025-02-15 14:14:42 +0700

    init

---
+++ services/order/src/main/java/com/alibou/ecommerce/order/Order.java
@@ -0,0 +1,49 @@
+package com.alibou.ecommerce.order;
+
+
+import com.alibou.ecommerce.orderline.OrderLine;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.EnumType.STRING;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String reference;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(STRING)
+    private PaymentMethod paymentMethod;
+
+    private String customerId;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createAt;
+
+    @CreatedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
---
+++ services/order/src/main/java/com/alibou/ecommerce/order/PaymentMethod.java
@@ -0,0 +1,12 @@
+package com.alibou.ecommerce.order;
+
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeVisAnnos;
+
+public enum PaymentMethod {
+
+    PAYPAL,
+    CREDIT_CARD,
+    VISA,
+    MASTER_CARD,
+    BITCOIN
+}
